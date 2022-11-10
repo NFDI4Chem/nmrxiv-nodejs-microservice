f0f91c0eeb660181dc116fa5dd0b50fce61af4ae
hamed-musallam <hamed.musallam@gmail.com>
2022-11-10 13:15:03 +0100

    test: fix API test

--- tests/functional/spectra.spec.ts
+++ tests/functional/spectra.spec.ts
@@ -4,17 +4,18 @@ test.group('Spectra', () => {
   test('should parse spectra from URLs and return their snapshots', async ({ client }) => {
     const response = await client.post('/spectra-parser').json({
       urls: [
-        'https://cheminfo.github.io/bruker-data-test/data/zipped/aspirin-1h.zip',
-        'https://nmrxiv.org/NRayya/datasets/fsu-nmr-platform-samples/cucurbitacin-e/12',
+        // 'https://cheminfo.github.io/nmr-dataset-demo/cytisine/13c.jdx',
+        'https://cheminfo.github.io/nmr-dataset-demo/cytisine/1h.jdx',
+        'https://cloud.uni-jena.de/s/y72GbCX8bJbmpJT/download/10.zip',
+        'https://cloud.uni-jena.de/s/jsMed9fmqWZzo6r/download/53.zip',
       ],
       snapshot: true,
     })
     response.assertStatus(200)
 
     const { spectra, molecules, images } = response.body()
-    response.assert?.equal(spectra?.length, 3)
-    response.assert?.equal(molecules?.length, 0)
+    response.assert?.equal(spectra?.length, 5)
+    response.assert?.equal(molecules?.length, 2)
     response.assert?.equal(images?.length, 3)
   })
-  // Write your test here
 })
